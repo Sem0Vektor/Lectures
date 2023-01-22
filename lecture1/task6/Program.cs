4edc1889a3e19347db3d213ee4ced3ef457fa8c5
Sem0Vektor <mozziella@proton.me>
2023-01-22 17:31:06 +0500

    task6

---
+++ lecture1/task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter username ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "sem")
+{
+    Console.WriteLine("Nice Dick, Awesome Balls");
+}
+else
+{
+    Console.WriteLine("Hello, " +username +"!");
+}
